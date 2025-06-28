207104a94a4826ebb0e551a115609fe3836f63e6
Dalton Ricker <40616938+SasquatchXYZ@users.noreply.github.com>
2025-06-28 11:56:10 -0400

    Add shell sort
    array[j] = ai;

---
+++ SortingAlgorithms/ShellSort.cs
@@ -0,0 +1,23 @@
+namespace SortingAlgorithms;
+
+public static class ShellSort
+{
+    public static void Sort(int[] array)
+    {
+        for (var h = array.Length / 2; h > 0; h /= 2)
+        {
+            for (var i = h; i < array.Length; i++)
+            {
+                var j = i;
+                var ai = array[i];
+                while (j >= h && array[j - h] > ai)
+                {
+                    array[j] = array[j - h];
+                    j -= h;
+                }
+
+                array[j] = ai;
+            }
+        }
+    }
+}
