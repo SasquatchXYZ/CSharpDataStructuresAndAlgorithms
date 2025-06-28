c61c34cc0a21a24accea6fc92fdba40616d66906
Dalton Ricker <40616938+SasquatchXYZ@users.noreply.github.com>
2025-06-27 21:56:42 -0400

    Add selection sort and project for testing

---
+++ Chapter03Playground/Program.cs
@@ -0,0 +1,5 @@
+﻿using SortingAlgorithms;
+
+int[] testArray = [-11, 12, -42, 0, 1, 90, 68, 6, -9];
+SelectionSort.Sort(testArray);
+Console.WriteLine(string.Join(" | ", testArray));
---
+++ SortingAlgorithms/SelectionSort.cs
@@ -0,0 +1,23 @@
+﻿namespace SortingAlgorithms;
+
+public static class SelectionSort
+{
+    public static void Sort(int[] array)
+    {
+        for (var i = 0; i < array.Length - 1; i++)
+        {
+            var minIndex = i;
+            var minValue = array[i];
+            for (var j = i + 1; j < array.Length; j++)
+            {
+                if (array[j] < minValue)
+                {
+                    minIndex = j;
+                    minValue = array[j];
+                }
+            }
+
+            (array[i], array[minIndex]) = (array[minIndex], array[i]);
+        }
+    }
+}
