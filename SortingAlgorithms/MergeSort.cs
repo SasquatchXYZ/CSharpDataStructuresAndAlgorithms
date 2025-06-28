0472f326248fb58aca6b11936fcc6070ab3d8583
Dalton Ricker <40616938+SasquatchXYZ@users.noreply.github.com>
2025-06-28 00:06:23 -0400

    Add merge sort

---
+++ SortingAlgorithms/MergeSort.cs
@@ -0,0 +1,48 @@
+namespace SortingAlgorithms;
+
+public static class MergeSort
+{
+    public static void Sort(int[] array)
+    {
+        if (array.Length <= 1) return;
+
+        var middle = array.Length / 2;
+        var left = GetSubarray(array, 0, middle - 1);
+        var right = GetSubarray(array, middle, array.Length - 1);
+
+        Sort(left);
+        Sort(right);
+
+        int i = 0, j = 0, k = 0;
+        while (i < left.Length && j < right.Length)
+        {
+            if (left[i] <= right[j])
+            {
+                array[k] = left[i++];
+            }
+            else
+            {
+                array[k] = right[j++];
+            }
+
+            k++;
+        }
+
+        while (i < left.Length)
+        {
+            array[k++] = left[i++];
+        }
+
+        while (j < right.Length)
+        {
+            array[k++] = right[j++];
+        }
+    }
+
+    private static int[] GetSubarray(int[] array, int start, int end)
+    {
+        var result = new int[end - start + 1];
+        Array.Copy(array, start, result, 0, end - start + 1);
+        return result;
+    }
+}
