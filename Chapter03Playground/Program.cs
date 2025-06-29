352285a814909a1f06f7dfd59159b6023ba548e6
Dalton Ricker <40616938+SasquatchXYZ@users.noreply.github.com>
2025-06-29 15:06:22 -0400

    Testing the sorting algorithms

--- Chapter03Playground/Program.cs
+++ Chapter03Playground/Program.cs
@@ -2,22 +2,30 @@
 using System.Diagnostics;
 using SortingAlgorithms;
 
-int[] testArray = [-11, 12, -42, 0, 1, 90, 68, 6, -9];
-
-List<AbstractSort> algorithms = new()
-{
+List<AbstractSort> algorithms =
+[
     new SelectionSort(),
     new InsertionSort(),
     new BubbleSort(),
     new MergeSort(),
     new ShellSort(),
     new Quicksort(),
-    new HeapSort(),
-};
+    new HeapSort()
+];
+
+const string path = "../../../SortingAlgorithmTesting.md";
+
+var fs = new FileStream(path, FileMode.Create);
+var sw = new StreamWriter(fs);
+
+Console.WriteLine($"\nStarting Sorting Algorithms Testing at {DateTime.Now}");
+sw.WriteLine($"\n# Sorting Algorithms Testing");
+sw.WriteLine($"## {DateTime.Now:D}");
 
 for (var n = 0; n <= 100_000; n += 10_000)
 {
-    Console.WriteLine($"\nRunning tests for n = {n}:");
+    Console.WriteLine($"\nCurrently Running tests for n = {n:N0}:");
+    sw.WriteLine($"\n## Results for n = {n:N0}:");
     List<(Type Type, long Ms)> milliseconds = [];
     for (var i = 0; i < 5; i++)
     {
@@ -42,6 +50,12 @@ for (var n = 0; n <= 100_000; n += 10_000)
 
     foreach (var (type, avg) in results)
     {
-        Console.WriteLine($"{type.Name}: {avg} ms");
+        sw.WriteLine($"{type.Name}: {avg} ms");
     }
 }
+
+sw.Flush();
+sw.Close();
+fs.Close();
+
+Console.WriteLine($"Sorting Algorithms Testing finished at {DateTime.Now}");
