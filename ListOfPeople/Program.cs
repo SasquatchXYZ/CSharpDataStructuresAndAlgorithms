8532ed526e88f0aa7c200093903e4bf762c67e6e
Dalton Ricker <40616938+SasquatchXYZ@users.noreply.github.com>
2025-06-29 21:35:46 -0400

    Add List of People example

---
+++ ListOfPeople/Program.cs
@@ -0,0 +1,15 @@
+﻿using ListOfPeople;
+
+List<Person> people =
+[
+    new("Marcin", 35, "PL"),
+    new("Sabine", 25, "DE"),
+    new("Mark", 31, "PL"),
+];
+
+var sortedPeople = people.OrderBy(person => person.Name).ToList();
+
+foreach (var person in sortedPeople)
+{
+    Console.WriteLine($"{person.Name} ({person.Age}) from {person.Country}.");
+}
