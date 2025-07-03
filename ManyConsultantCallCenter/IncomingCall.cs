88a03ab6d45c0385e8c2665aa78c5882c49c407f
Dalton Ricker <40616938+SasquatchXYZ@users.noreply.github.com>
2025-07-03 00:07:38 -0400

    Add ManyConsultantCallCenter (concurrentQueue) example

---
+++ ManyConsultantCallCenter/IncomingCall.cs
@@ -0,0 +1,11 @@
+namespace ManyConsultantCallCenter;
+
+public class IncomingCall
+{
+    public int Id { get; set; }
+    public int ClientId { get; set; }
+    public DateTime CallTime { get; set; }
+    public DateTime? AnswerTime { get; set; }
+    public DateTime? EndTime { get; set; }
+    public string? Consultant { get; set; }
+}
---
+++ ManyConsultantCallCenter/Program.cs
@@ -0,0 +1,9 @@
+﻿using ManyConsultantCallCenter;
+
+var center = new CallCenter();
+var ops = new Operations();
+Parallel.Invoke(
+    () => ops.Clients(center),
+    () => ops.Consultant(center, "Marcin", ConsoleColor.Red),
+    () => ops.Consultant(center, "James", ConsoleColor.Yellow),
+    () => ops.Consultant(center, "Olivia", ConsoleColor.Green));
