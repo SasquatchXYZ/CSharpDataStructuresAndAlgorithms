4954abeb3b959c7be7dd4ce44a29c0587d482920
Dalton Ricker <40616938+SasquatchXYZ@users.noreply.github.com>
2025-07-04 16:38:38 -0400

    Implement the GravityRollerCoaster

--- GravityRollerCoaster/Program.cs
+++ GravityRollerCoaster/Program.cs
@@ -1,3 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using static GravityRollerCoaster.Utilities;
+using GravityRollerCoaster;
+using QueueItem = (System.DateTime StartedAt, System.ConsoleColor Color);
 
-Console.WriteLine("Hello, World!");
+const int rideSeconds = 10;
+var random = new Random();
+var queue = new CircularQueue<QueueItem>(12);
+var color = ConsoleColor.Black;
+
+while (true)
+{
+    while (queue.Peek() is not null)
+    {
+        var item = queue.Peek()!.Value;
+        var elapsed = DateTime.Now - item.StartedAt;
+        if (elapsed.TotalSeconds < rideSeconds)
+            break;
+
+        queue.Dequeue();
+        Log($"> Exits\tTotal: {queue.Count}", item.Color);
+    }
+
+    var isNew = random.Next(3) == 1;
+    if (isNew)
+    {
+        color = color == ConsoleColor.White
+            ? ConsoleColor.DarkBlue
+            : (ConsoleColor) ((int) color + 1);
+
+        if (queue.Enqueue((DateTime.Now, color)))
+        {
+            Log($"< Enters\tTotal: {queue.Count}", color);
+        }
+        else
+        {
+            Log($"! Not allowed\tTotal: {queue.Count}", ConsoleColor.DarkGray);
+        }
+    }
+
+    await Task.Delay(500);
+}
