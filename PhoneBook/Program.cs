5f0501814a683947b3352d1a337392c60fe0786b
Dalton Ricker <40616938+SasquatchXYZ@users.noreply.github.com>
2025-07-04 18:30:18 -0400

    Add PhoneBook (Hashtable) example

---
+++ PhoneBook/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+
+var phoneBook = new Hashtable
+{
+    {
+        "Marcin", "101-202-303"
+    },
+    {
+        "Martyna", "202-303-404"
+    },
+};
+
+phoneBook["Aline"] = "303-404-505";
+
+Console.WriteLine("Phone numbers:");
+if (phoneBook.Count == 0)
+    Console.WriteLine("Empty list.");
+
+foreach (DictionaryEntry entry in phoneBook)
+{
+    Console.WriteLine($"{entry.Key}: {entry.Value}");
+}
+
+Console.Write("\nSearch by name: ");
+var name = Console.ReadLine() ?? string.Empty;
+if (phoneBook.ContainsKey(name))
+{
+    var number = (string) phoneBook[name]!;
+    Console.WriteLine($"Phone number for {name}: {number}");
+}
+else
+{
+    Console.WriteLine($"No phone number for {name}.");
+}
