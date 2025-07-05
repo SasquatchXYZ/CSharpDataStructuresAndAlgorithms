c28f43e14ed39f445aed56c4f0221d714fa93762
Dalton Ricker <40616938+SasquatchXYZ@users.noreply.github.com>
2025-07-05 14:25:07 -0400

    Add coupon (HashSet) example

---
+++ Coupons/Program.cs
@@ -0,0 +1,25 @@
+﻿var usedCoupons = new HashSet<int>();
+do
+{
+    Console.Write("Enter the number: ");
+    var number = Console.ReadLine() ?? string.Empty;
+
+    if (!int.TryParse(number, out var coupon))
+        break;
+
+    if (usedCoupons.Contains(coupon))
+    {
+        Console.WriteLine("Invalid coupon - Already used.");
+    }
+    else
+    {
+        usedCoupons.Add(coupon);
+        Console.WriteLine("Coupon accepted - Thank you!");
+    }
+} while (true);
+
+Console.WriteLine("\nUsed coupons:");
+foreach (var coupon in usedCoupons)
+{
+    Console.WriteLine(coupon);
+}
