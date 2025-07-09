2e918233b0220dcfbbb438e428cb9e773fa71649
Dalton Ricker <40616938+SasquatchXYZ@users.noreply.github.com>
2025-07-08 22:39:35 -0400

    Add the example projects for graphs

---
+++ DirectedWeightedEdges/Program.cs
@@ -0,0 +1,28 @@
+﻿using GraphDatastructures;
+
+var graph = new Graph<int> { IsDirected = true, IsWeighted = true };
+
+var n1 = graph.AddNode(1);
+var n2 = graph.AddNode(2);
+var n3 = graph.AddNode(3);
+var n4 = graph.AddNode(4);
+var n5 = graph.AddNode(5);
+var n6 = graph.AddNode(6);
+var n7 = graph.AddNode(7);
+var n8 = graph.AddNode(8);
+
+graph.AddEdge(n1, n2, 9);
+graph.AddEdge(n1, n3, 5);
+graph.AddEdge(n2, n1, 3);
+graph.AddEdge(n2, n4, 18);
+graph.AddEdge(n3, n4, 12);
+graph.AddEdge(n4, n2, 2);
+graph.AddEdge(n4, n8, 8);
+graph.AddEdge(n5, n4, 9);
+graph.AddEdge(n5, n6, 2);
+graph.AddEdge(n5, n7, 5);
+graph.AddEdge(n5, n8, 3);
+graph.AddEdge(n6, n7, 1);
+graph.AddEdge(n7, n5, 4);
+graph.AddEdge(n7, n8, 6);
+graph.AddEdge(n8, n5, 3);
---
+++ UndirectedUnweightedEdges/Program.cs
@@ -0,0 +1,23 @@
+﻿using GraphDatastructures;
+
+var graph = new Graph<int> { IsDirected = false, IsWeighted = false };
+
+var n1 = graph.AddNode(1);
+var n2 = graph.AddNode(2);
+var n3 = graph.AddNode(3);
+var n4 = graph.AddNode(4);
+var n5 = graph.AddNode(5);
+var n6 = graph.AddNode(6);
+var n7 = graph.AddNode(7);
+var n8 = graph.AddNode(8);
+
+graph.AddEdge(n1, n2);
+graph.AddEdge(n1, n3);
+graph.AddEdge(n2, n4);
+graph.AddEdge(n3, n4);
+graph.AddEdge(n4, n5);
+graph.AddEdge(n5, n6);
+graph.AddEdge(n5, n7);
+graph.AddEdge(n5, n8);
+graph.AddEdge(n6, n7);
+graph.AddEdge(n7, n8);
